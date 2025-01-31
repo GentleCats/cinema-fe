c33eb615d81e092cc580291e989667a090326db1
max655 <147141510+max655@users.noreply.github.com>
2025-01-31 19:10:05 +0200

    change to axios

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import ListFilm from "@/components/FilmList";
 import { Film } from "@/models/Film";
 import { Container, Typography } from "@mui/material";
+import axios from "axios";
 
 const Home: React.FC = () => {
   const [films, setFilms] = useState<Film[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:5092/api/Movie/get-popular?page=1")
-      .then((res) => res.json())
-      .then((data) => setFilms(data))
+    axios.get("http://localhost:5092/api/Movie/get-popular?page=1")
+      .then((res) => setFilms(res.data))
       .catch((err) => console.error("Failed to fetch films", err));
   }, []);
 
